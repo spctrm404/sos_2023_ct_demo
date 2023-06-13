a5f3ac07c9dde3bc384a490fbc08e0e8dea53eb6
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2023-06-14 01:37:28 +0900

    프로젝트 막 마무리 됨 업로드 준비 완료

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,23 @@
+const reference = [
+  {
+    author: "lena polishko",
+    title: "",
+    year: "2023",
+    sourceTitle: "",
+    link: "https://unsplash.com/@ilon_a",
+  },
+  {
+    author: "ash thorp",
+    title: "M E C H A - YouTube",
+    year: "2021",
+    sourceTitle: "",
+    link: "https://vimeo.com/486708402",
+  },
+  {
+    author: "James Hoffmann",
+    title: "Is The Future Of The AeroPress Clear?",
+    year: "2023",
+    sourceTitle: "",
+    link: "https://www.youtube.com/watch?v=NWvU-MncnJI",
+  },
+];
